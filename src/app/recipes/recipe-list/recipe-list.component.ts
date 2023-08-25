e936351d85998fb001d9064606e34c9220d26fdb
baraaaboshi123 <baraa5hfg@gmail.com>
2023-08-25 10:28:26 +0300

    add recipes and recipes details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes : Recipe[] =[
+    new Recipe("A test recipe", 'this is a test recipe', 'http://blog.thefruitcompany.com/wp-content/uploads/2012/12/IMG_1443-1024x764.jpg'),
+    new Recipe("A second test recipe","this is second test recipe","https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/1:1/w_1920,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg")
+  ];
 
 }
